6a3d4c320317b230f35f8aeee0d80e840c987307
Abhijit Kumar <thefanofvirat@gmail.com>
2020-08-10 15:00:45 +0530

    initial commit

---
+++ src/app/comment/comment.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-comment',
+  templateUrl: './comment.component.html',
+  styleUrls: ['./comment.component.css']
+})
+export class CommentComponent implements OnInit {
+
+
+
+  comment = " ";
+  postComment = [];
+
+post()
+{
+  this.postComment.push(this.comment);
+  this.comment = "";
+}
+
+
+  constructor() { }
+
+  ngOnInit(){
+  }
+
+}
